2aaae75611d281e0cd57bb8eb2eb1ba04a60d791
aolesek <arkadiusz.olesek@gmail.com>
2018-02-02 19:35:06 +0100

    przyklad gui z jednym handlerem, nie mam pojecia jak aktualizaowac to co w gui jest co chwile, to chyba jest trudne elo xd

--- src/main.adb
+++ src/main.adb
@@ -1,188 +1,94 @@
-with Ada.Text_IO;
-use Ada.Text_IO;
-with Ada.Calendar;  use Ada.Calendar;
-with Ada.Numerics.Elementary_Functions;
-with ada.numerics.float_random;
+with Gtk.Box;         use Gtk.Box;
+with Gtk.Label;       use Gtk.Label;
+with Gtk.Widget;      use Gtk.Widget;
+with Gtk.Main;
+with Gtk.Window;      use Gtk.Window;
+with Gtk;            use Gtk;
+with Glib.Error;     use Glib.Error;
+with Gtkada.Builder; use Gtkada.Builder;
+with Ada.Text_IO;    use Ada.Text_IO;
+with Glib; use Glib;
+with Handlers;
+with Gtk.Button; use Gtk.Button;
+
 
 
 procedure Main is
 
-   Gen : ada.numerics.float_random.Generator;
 
-   MasaAuta : Float := 900.0;
-   PrzyspieszenieZiemskie : Float := 10.0;
-   WspoczynnikPodloza : Float := 0.7 with Atomic;
+   Win   : Gtk_Window;
+   Label : Gtk_Label;
+   Box   : Gtk_Vbox;
+   Builder       : Gtkada_Builder;
+   Return_Code : Guint;
+   Error       : aliased Glib.Error.GError;
+   Button : Gtk_Button;
 
-   type DaneWspoldzielone is record
-      Prefix: String(1 .. 9);
-      CzyHamulecWcisniety : Boolean;
-      SilaHamowania : Float;
-      PredkoscAuta : Float;
-      ABSwlaczony: Boolean;
-   end record;
+Migration_Interval    : Glib.Guint := 1000;
 
-   type Dane_Access is access DaneWspoldzielone;
+   procedure BrakeOn (Button : access Gtk_Button_Record'Class) is
+begin
+         Put_Line("brake on");
 
-   procedure ObliczaniePredkosci(Prefix: in String; PredkoscAuta: in out Float; SilaHamowania: in out Float) is
-      EnergiaPoczatkowa : Float :=0.0;
-      DrogaPrzebyta : Float;
-      PredkoscInteger : Integer;
-      Zmienna : Float;
+   end BrakeOn;
+
+    function Migrate( Number_To_Move : in Integer ) return Boolean is
+
    begin
-      EnergiaPoczatkowa := 0.5*MasaAuta*PredkoscAuta*PredkoscAuta;
-      -- obliczamy droge przebyta przez 1 sekunde
-      DrogaPrzebyta := PredkoscAuta/10.0;
-      -- obliczamy predkosc po jednej sekundzie dzialania sily hamowania
-
-      Zmienna :=  EnergiaPoczatkowa-SilaHamowania*DrogaPrzebyta;
-      if Zmienna >= 0.0 then
-         PredkoscAuta := Ada.Numerics.Elementary_Functions.Sqrt(2.0*(Zmienna)/MasaAuta); -- to powinno byc w pierwiastku
-      else
-         PredkoscAuta := 0.0;
-      end if;
-
-      PredkoscInteger := Integer(PredkoscAuta);
-      Put_Line(Prefix & PredkoscInteger'Img);
-   end ObliczaniePredkosci;
-
-   function LosoweZmianyPodloza(WspolczynnikOryginalny: in Float) return Float is
-      RandomFloat: Float;
-      ZmianaPodloza: Float;
-   begin
-      RandomFloat := Ada.Numerics.Float_Random.Random(Gen);
-      RandomFloat := RandomFloat - 0.5;
-      ZmianaPodloza := RandomFloat/10.0;
-      --Put_Line("Zmiana podloza" & ZmianaPodloza'Img);
-      return WspolczynnikOryginalny+ZmianaPodloza;
-   end LosoweZmianyPodloza;
-
-   -- zmniejsza/zwieksza sile hamowania
-   -- na podstawie sygnalow z czujnika
-   -- wejsciem info czy kola zablokowane
-   task type SterownikABS(Dane: Dane_Access) is
-   end SterownikABS;
-
-   -- dostaje sygnal ze hamulec zostal nacisniety
-   -- po tym sprawdza co ustalony czas czy kola sie nie zablokowaly
-   -- oblicza aktualny wpsolczynnik tarcia statycznego
-   -- sila hamulca przez m*g
-   -- if aktualny_wsp>podloze_wsp => kola zablokowane
-   -- wejsciem moze byc boolean czyHamulecNacisniety
-   task type Czujnik(Dane: Dane_Access) is
-   end Czujnik;
-
-   -- po prostu hamulec
-   -- na poczatku sila stala
-   -- po wlaczeniu ABSu zmieniamy wedlug zalecen ABSu
-   -- wejscie sila z jaka hamujemy
-   -- wysyla sygnal do czujnika ze dziala
-   task type Hamulec(Dane: Dane_Access) is
-      entry Start;
-   end Hamulec;
-
-   task body Hamulec is
-   begin
-      accept Start do
-         Put_Line(Dane.Prefix & "hamulec uruchomiony");
-         Dane.SilaHamowania := 8000.0; -- mozna zakomentowac i podawac na samym poczatku
-         Dane.CzyHamulecWcisniety := True;
-         delay 0.2;
-         while Dane.PredkoscAuta > 0.0 and Dane.ABSwlaczony = False loop
-            Put_Line(Dane.Prefix & "hamowanie, ABS nieaktywny");
-            --obliczamy energie poczatkowa
-            ObliczaniePredkosci(Dane.Prefix, Dane.PredkoscAuta, Dane.SilaHamowania);
-            delay 0.1;
-         end loop;
-      end Start;
-   end Hamulec;
-
-   task body Czujnik is
-      AktualnyWspolczynnik : Float := 0.0;
-   begin
-      Put_Line(Dane.Prefix & "czujnik zainicjalizowany");
-
-         while Dane.CzyHamulecWcisniety = false loop
-         --delay 0.1;
-         null;
-         end loop;
-
-         Put_Line(Dane.Prefix & "czujnik kola aktywowany");
-         while Dane.PredkoscAuta > 0.0 and Dane.ABSwlaczony = false loop
-            -- co sekunde sprawdza czy kola sie nie zablokowaly
-            -- sila hamowania przez m*g = aktualny_wsp
-            AktualnyWspolczynnik := Dane.SilaHamowania/(PrzyspieszenieZiemskie*MasaAuta);
-            -- if aktualny_wsp>podloze_wsp => kola zablokowane
-            if AktualnyWspolczynnik>LosoweZmianyPodloza(WspoczynnikPodloza) then
-               Put_Line(Dane.Prefix & "kolo zablokowane");
-               Dane.ABSwlaczony := True;
-            end if;
-            -- wlacz abs
-         end loop;
-         -- dopoki aktualny_wsp>podloze_wsp zmniejszaj sile
-
-         Put_Line(Dane.Prefix & "czujnik kola dezaktywowany");
-   end Czujnik;
-
-   task body SterownikABS is
-      AktualnyWspolczynnik : Float := 0.8;
-   begin
-
-      Put_Line(Dane.Prefix & "sterownik zainicjalizowany");
-
-      while Dane.ABSwlaczony = false loop
-         --delay 0.1;
-                  null;
-         end loop;
-      Put_Line(Dane.Prefix & "ABS aktywowany");
-
-      --dopoki predkosc nie jest zero
-      while Dane.PredkoscAuta > 0.0 loop
-         -- dobieramy odpowiednia sile
-         Dane.SilaHamowania := Dane.SilaHamowania+200.0;
-         while AktualnyWspolczynnik>WspoczynnikPodloza loop
-            Dane.SilaHamowania := Dane.SilaHamowania-100.0;
-            AktualnyWspolczynnik := Dane.SilaHamowania/(PrzyspieszenieZiemskie*MasaAuta);
-            --Put_Line("in");
-         end loop;
-
-         ObliczaniePredkosci(Dane.Prefix, Dane.PredkoscAuta, Dane.SilaHamowania);
-         delay 0.1;
-
-      end loop;
-         Put_Line(Dane.Prefix &  "ABS dezaktywowany");
-   end SterownikABS;
+      Put_Line("elo" & Number_To_Move'Img);
+      return True; -- "false" will disable further timeouts on this.
+   end Migrate;
 
 
 
-   --LP - lewy przód PP - prawy przód LT PT
-   Dane_LP: Dane_Access := new DaneWspoldzielone'(ASCII.ESC & "[30m" & "LP: ",False,0.0,15.0,False);
-   S_LP : SterownikABS(Dane_LP);
-   Cz_LP : Czujnik(Dane_LP);
-   H_LP : Hamulec(Dane_LP);
-
-   Dane_PP: Dane_Access := new DaneWspoldzielone'(ASCII.ESC & "[31m" & "PP: ",False,0.0,25.0,False);
-   S_PP : SterownikABS(Dane_PP);
-   Cz_PP : Czujnik(Dane_PP);
-   H_PP : Hamulec(Dane_PP);
-
-   Dane_LT: Dane_Access := new DaneWspoldzielone'(ASCII.ESC & "[32m" & "LT: ",False,0.0,35.0,False);
-   S_LT : SterownikABS(Dane_LT);
-   Cz_LT : Czujnik(Dane_LT);
-   H_LT : Hamulec(Dane_LT);
-
-   Dane_PT: Dane_Access := new DaneWspoldzielone'(ASCII.ESC & "[34m" & "PT: ",False,0.0,45.0,False);
-   S_PT : SterownikABS(Dane_PT);
-   Cz_PT : Czujnik(Dane_PT);
-   H_PT : Hamulec(Dane_PT);
 
 begin
-   Put_Line("START#############################");
-   delay 3.0;
-   H_LP.Start;
-   H_PP.Start;
-   H_LT.Start;
-   H_PT.Start;
 
-   --powrót do czarnego koloru terminala
-   Put_Line(ASCII.ESC & "[30m");
+   Gtk.Main.Init;
+Gtk_New (Builder);
+
+-- Load `arun.glade` which contains the interface definition
+Return_Code := Add_From_File (Builder  => Builder,
+                              Filename => "gui.glade",
+                              Error    => Error'Access);
+
+if Error /= null then
+   Put_Line ("Error : " & Get_Message (Error));
+   Error_Free (Error);
+   return;
+end if;
+
+-- Register a signal handler for when
+-- the "Main_Quit" signal is fired
+
+
+Do_Connect (Builder);
+
+-- Get the Gtk_Widget object from the Gtkada.Builder API
+-- for our commandWindow so it will be shown.
+
+   Button := Gtk_Button (Builder.Get_Object ("brakeOn"));
+   Button.On_Clicked (BrakeOn'Unrestricted_Access);
+   Button.Set_Label("Elo");
+
+  --  Timeout_Callback.Add( Interval => Migration_Interval,
+  --                                         Func     => Migrate'Access,
+   --                                        D        => 1000 );
+
+
+
+Gtk.Widget.Show_All (
+    Gtk_Widget (Gtkada.Builder.Get_Object (Builder, "monitorWindow")));
+
+   Gtk.Main.Main;
+
+   Endless_Loop :
+   loop
+
+      Button.Set_Label("Ty kurwo jebana");
+
+   end loop Endless_Loop;
+
+--Unref (Builder);
+
 end Main;
